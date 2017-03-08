35539d30eed0e6ff1d5eac83eb4194702e49b643
Limeiyuan <ojwftded@gmail.com>
2017-03-08 10:31:54 +0800

    页面展示调整

--- src/main/java/com/li/exchange/controller/HomeController.java
+++ src/main/java/com/li/exchange/controller/HomeController.java
@@ -29,14 +29,8 @@ public class HomeController {
 
     @RequestMapping("getRate")
     @ResponseBody
-    public List<RateDTO> getRate(String name) {
-        if (StringUtils.isEmpty(name)) {
-            name = "人民币";
-        }
-        Rate dest = rateService.getByName(name);
-        if (dest == null) {
-            dest = rateService.getByName("人民币");
-        }
+    public List<RateDTO> getRate() {
+        String name = "人民币";
         List<Rate> rates = rateService.query(null);
         List<RateDTO> rateDTOS = new ArrayList<>();
         for (Rate rate : rates) {
@@ -49,8 +43,8 @@ public class HomeController {
             RateDTO dto = new RateDTO();
             dto.setName(name + "/" + rate.getName());
             dto.setTitle(name + "/" + rate.getName());
-            dto.setIn(Formater.formatDouble(dest.getBought_in() / rate.getBought_in(), 4));
-            dto.setOut(Formater.formatDouble(dest.getBought_out() / rate.getBought_out(), 4));
+            dto.setIn(Formater.formatDouble(rate.getBought_in(), 4));
+            dto.setOut(Formater.formatDouble(rate.getBought_out(), 4));
             dto.setRate(Formater.formatDouble(rate.getRate(), 4));
             rateDTOS.add(dto);
         }
